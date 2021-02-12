250237717e591d8d3e8efa1541eef13286543b14
Mathieu Fréchette <frechette.mathieu@cegepvicto.ca>
2021-02-12 10:01:25 -0500

    Ajustement commentaire et nb fichier de logs

--- src/Factory/LoggerFactory.php
+++ src/Factory/LoggerFactory.php
@@ -84,7 +84,7 @@ final class LoggerFactory
     public function addFileHandler(string $filename, int $level = null): self
     {
         $filename = sprintf('%s/%s', $this->path, $filename);
-        $rotatingFileHandler = new RotatingFileHandler($filename, 0, $level ?? $this->level, true, 0777);
+        $rotatingFileHandler = new RotatingFileHandler($filename, 7, $level ?? $this->level, true, 0777);
 
         // The last "true" here tells monolog to remove empty []'s
         $rotatingFileHandler->setFormatter(new LineFormatter(null, null, false, true));
