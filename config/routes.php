36ac6713bebf88ea40f3bb2ce3092cfeed9ac94c
Mathieu Fréchette <frechette.mathieu@cegepvicto.ca>
2021-02-19 22:15:35 -0500

    Supprime la route create user

--- config/routes.php
+++ config/routes.php
@@ -5,9 +5,8 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 return function (App $app) {
+    
     $app->get('/', \App\Action\Docs\SwaggerUiAction::class)->setName('home');
-
-    $app->post('/users', \App\Action\UserCreateAction::class);
 
     // Documentation
     $app->get('/docs/v1', \App\Action\Docs\SwaggerUiAction::class);
